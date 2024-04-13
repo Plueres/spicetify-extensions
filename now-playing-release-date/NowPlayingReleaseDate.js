e6cbe7fa8abd032dfc8b4d7928eadf3006eb8254
Rick <S1204255@student.windesheim.nl>
2024-04-14 00:17:55 +0200

    Added the files from the previous repository

---
+++ now-playing-release-date/NowPlayingReleaseDate.js
@@ -0,0 +1,61 @@
+// Wait for 2 second before running the script to ensure that Spicetify has loaded.
+setTimeout(() => initialize(), 2000);
+
+async function initialize() {
+    try {
+        await waitForSpicetify();
+        Spicetify.Player.addEventListener("songchange", async () => {
+            removeExistingReleaseDateElement(); // Remove the release date element immediately when the song changes.
+            await displayReleaseDate();
+        });
+        await displayReleaseDate(); // Retrieve the release date on initial load.
+    } catch (error) {
+        console.error('Error initializing:', error);
+    }
+}
+
+async function waitForSpicetify() {
+    while (!Spicetify || !Spicetify.showNotification) {
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
+}
+
+async function displayReleaseDate() {
+    try {
+        const trackId = Spicetify.Player.data.item.uri.split(":")[2];
+        const trackDetails = await Spicetify.CosmosAsync.get(`https://api.spotify.com/v1/tracks/${trackId}`);
+        const releaseDate = new Date(trackDetails.album.release_date);
+        const formattedReleaseDate = `${String(releaseDate.getDate()).padStart(2, '0')}-${String(releaseDate.getMonth() + 1).padStart(2, '0')}-${releaseDate.getFullYear()}`;
+
+        removeExistingReleaseDateElement();
+
+        // Wait for 1 second before displaying the release date.
+        setTimeout(() => {
+            const releaseDateElement = createReleaseDateElement(formattedReleaseDate);
+            const container = document.querySelector('.main-trackInfo-artists');
+            container.appendChild(releaseDateElement);
+        }, 800);
+    } catch (error) {
+        console.error('Error displaying release date:', error);
+    }
+}
+
+function removeExistingReleaseDateElement() {
+    const existingReleaseDateElement = document.getElementById('releaseDate');
+    if (existingReleaseDateElement) {
+        existingReleaseDateElement.remove();
+    }
+}
+
+function createReleaseDateElement(formattedReleaseDate) {
+    const releaseDateElement = document.createElement("div");
+    releaseDateElement.id = 'releaseDate';
+    releaseDateElement.textContent = ` • ${formattedReleaseDate}`;
+
+    const artistElement = document.querySelector('.main-trackInfo-artists a');
+    const artistStyles = window.getComputedStyle(artistElement);
+    releaseDateElement.style.fontSize = artistStyles.fontSize;
+    releaseDateElement.style.fontWeight = artistStyles.fontWeight;
+
+    return releaseDateElement;
+}
---
+++ now-playing-release-date/README.md
@@ -0,0 +1,4 @@
+# Now Playing Release Date
+
+![Preview image](https://github.com/Plueres/Spicetify-Modifications/blob/main/Extensions/now-playing-release-date/preview-small.jpg)
+This extension adds a release date for the currently playing track next to the artist name(s)
