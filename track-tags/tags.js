d4a07bc1cf23573d693874de79d96aaf84d81de6
Rick <S1204255@student.windesheim.nl>
2024-05-30 16:57:26 +0200

    Added more titles and removed redundant code

--- track-tags/tags.js
+++ track-tags/tags.js
@@ -118,44 +118,33 @@ async function displayTags() {
 
         // Check if the song is saved to liked songs collection
         if (savedTrack[0]) {
-            // Create a new span element
             const savedTrackSpan = document.createElement('span');
 
-            // Set the class of the span
             savedTrackSpan.setAttribute('class', 'Wrapper-sm-only Wrapper-small-only');
+            savedTrackSpan.setAttribute('title', 'This song is in your liked songs playlist');
 
-            // Create a new svg element
             const savedTrackSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-
-            // Set the attributes of the svg
             savedTrackSvg.setAttribute('role', 'img');
             savedTrackSvg.setAttribute('height', '24');
             savedTrackSvg.setAttribute('width', '24');
             savedTrackSvg.setAttribute('viewBox', '0 0 24 24');
             savedTrackSvg.setAttribute('class', 'Svg-img-icon-small-textBrightAccent playing-heart-tag');
 
-            // Create a new path element
             const savedTrackPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-
-            // Set the d attribute of the path
             savedTrackPath.setAttribute('d', 'M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z');
 
-            // Append the path to the svg
             savedTrackSvg.appendChild(savedTrackPath);
-
-            // Append the svg to the span
             savedTrackSpan.appendChild(savedTrackSvg);
 
-            // Append the span to the div
             tagsDiv.appendChild(savedTrackSpan);
         }
         // Check if the song is downloaded
         if (downloaded) {
-            // Create a new span element
             const downloadedSpan = document.createElement('span');
 
             downloadedSpan.setAttribute('class', 'encore-text encore-text-body-medium encore-internal-color-text-subdued main-trackList-rowBadges');
             downloadedSpan.setAttribute('data-encore-id', 'text');
+            downloadedSpan.setAttribute('title', 'This song is downloaded');
 
             const downloadedSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
             downloadedSvg.setAttribute('data-encore-id', 'icon');
@@ -164,12 +153,9 @@ async function displayTags() {
             downloadedSvg.setAttribute('viewBox', '0 0 16 16');
             downloadedSvg.setAttribute('class', 'Svg-sc-ytk21e-0 Svg-img-icon-small-textBrightAccent');
 
-            const downloadedTitle = document.createElementNS('http://www.w3.org/2000/svg', 'title');
-            downloadedTitle.textContent = 'Available offline';
             const downloadedPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
             downloadedPath.setAttribute('d', 'M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-4.75a.75.75 0 0 0-.75.75v5.94L6.055 8.744a.75.75 0 1 0-1.06 1.06L8 12.811l3.005-3.006a.75.75 0 1 0-1.06-1.06L8.75 9.939V4A.75.75 0 0 0 8 3.25z');
 
-            downloadedSvg.appendChild(downloadedTitle);
             downloadedSvg.appendChild(downloadedPath);
             downloadedSpan.appendChild(downloadedSvg);
 
@@ -178,16 +164,16 @@ async function displayTags() {
         // Check if the song is explicit
         if (trackDetails.explicit) {
             // Create a new span element
-            const explicitTag = document.createElement('span');
+            const explicitSpan = document.createElement('span');
 
             // Set the attributes and text content of the span
-            explicitTag.setAttribute('aria-label', 'Explicit');
-            explicitTag.setAttribute('class', 'main-tag-container playing-explicit-tag');
-            explicitTag.setAttribute('title', 'Explicit');
-            explicitTag.textContent = 'E';
+            explicitSpan.setAttribute('aria-label', 'Explicit');
+            explicitSpan.setAttribute('class', 'main-tag-container playing-explicit-tag');
+            explicitSpan.setAttribute('title', 'Warning!, This song is explicit and may contain strong language or themes.');
+            explicitSpan.textContent = 'E';
 
             // Append the span to the div
-            tagsDiv.appendChild(explicitTag);
+            tagsDiv.appendChild(explicitSpan);
         }
 
         // Append the div to Tagslist          
